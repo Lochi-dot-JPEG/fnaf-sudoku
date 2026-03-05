3fa6f16b6885b2bb71ea45e5de430594f95127b6
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-03-05 18:49:27 +1100

    feat: get player count with verification

--- player_management.py
+++ player_management.py
@@ -1,6 +1,20 @@
 import game
 
 def run_game():
+    # TODO turn this into a menu where you can choose how many with buttons
+    # 1, 2, 3, 4
+    player_count = 0
+    valid_player_count = False
+    while not valid_player_count:
+        try:
+            player_count = int(input("How many players are there?"))
+        except:
+            print("Enter a valid integer")
+            continue
+        valid_player_count = True
+
+    print("Player count is", player_count)
+
     result = game.play()
     if result.survived:
         print("Puzzle took: " + str(result.time) + " seconds")
